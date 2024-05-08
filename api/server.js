b127390e194d3da7b8939d6d26c65d5b1f08f674
Manuel Tovar <tovarmanuel569@gmail.com>
2024-05-08 16:25:27 -0500

    actualizacion de rutas

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/products*': '/'
+    '/api/*': '/$1',
 }))
 server.use(router)
 server.listen(3000, () => {
